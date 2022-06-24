e11357c3397eaa17ebdadf5fa327c19bdbcd18a4
Cahcastroc <camila.castronunes@gmail.com>
2022-06-24 13:39:49 -0300

    Edit activity and add Fragments

---
+++ app/src/main/java/br/edu/infnet/calculadora_imc_at/InputFragment.kt
@@ -0,0 +1,29 @@
+package br.edu.infnet.calculadora_imc_at
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+
+
+class InputFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_input, container, false)
+
+
+
+        return view
+    }
+
+
+
+}
