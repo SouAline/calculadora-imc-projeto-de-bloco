b7f3208267db3b3e09d1525ea7d28c460a470ea2
I_Igor Botani <igor.goncalves@mirante.net.br>
2022-06-27 20:57:15 -0300

    Calculo-imc

--- app/src/main/java/br/edu/infnet/calculadora_imc_at/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/calculadora_imc_at/MainActivity.kt
@@ -8,14 +8,27 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Comunicator{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       val inputFragment = InputFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView5, inputFragment).commit()
+
+    }
 
 
+    override fun passDataCom(imc: String, text: String) {
+        val bundle = Bundle()
+        bundle.putString("imc", imc)
+        bundle.putString("text",text)
 
+        val transaction = this.supportFragmentManager.beginTransaction()
+        val resultFragment = ResultFragment()
+        resultFragment.arguments = bundle
 
+        transaction.replace(R.id.fragmentContainerView5,resultFragment)
+        transaction.commit()
     }
 }
