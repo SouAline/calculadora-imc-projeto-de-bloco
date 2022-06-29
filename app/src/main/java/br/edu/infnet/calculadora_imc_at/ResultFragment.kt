7589871b220477092f634102ac9c187c0244a671
Julia Paz <juliapazpinheiro@gmail.com>
2022-06-29 21:02:46 +0100

    implementation of ideal weight

--- app/src/main/java/br/edu/infnet/calculadora_imc_at/ResultFragment.kt
+++ app/src/main/java/br/edu/infnet/calculadora_imc_at/ResultFragment.kt
@@ -12,8 +12,10 @@ class ResultFragment : Fragment() {
 
     private lateinit var imcResult: TextView
     private lateinit var textResult: TextView
+    private lateinit var idealResult: TextView
     var displayMessageImc: String? = ""
     var displayMessage: String? = ""
+    var displayPesoIdeal: String? = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +25,16 @@ class ResultFragment : Fragment() {
 
         displayMessageImc = arguments?.getString("imc")
         displayMessage = arguments?.getString("text")
+        displayPesoIdeal = arguments?.getString("pesoIdeal")
 
         imcResult = view.findViewById<TextView>(R.id.tvResultImc)
         textResult = view.findViewById<TextView>(R.id.tvResultAlert)
+        idealResult = view.findViewById<TextView>(R.id.tvIdealWeight)
 
 
         imcResult.setText(displayMessageImc)
         textResult.setText(displayMessage)
+        idealResult.setText(displayPesoIdeal)
 
         return view
     }
